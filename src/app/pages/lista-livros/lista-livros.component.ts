b3ac041ed0a520243133bb7105824b775f8f7eb9
ryanmulekim <ryanmulekim@gmail.com>
2024-04-12 21:23:26 -0300

    arrumando o readMe

--- src/app/pages/lista-livros/lista-livros.component.ts
+++ src/app/pages/lista-livros/lista-livros.component.ts
@@ -33,7 +33,7 @@ export class ListaLivrosComponent implements AfterViewInit{
   constructor(private service: LivroService) { }
 
   ngAfterViewInit(){
-    this.campoBuscaElement.nativeElement.focus();
+    this.campoBuscaElement.nativeElement.focus();   
   }
 
 
